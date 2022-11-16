e7e74d18d4b1481bb6ef839e681200cbe1d90824
crtnoo <91428713+crtnoo@users.noreply.github.com>
2022-11-16 09:33:31 -0300

    Add files via upload

---
+++ Crucigrama/Crucigrama/Crucigrama.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Crucigrama
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ButtonValidar = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ButtonValidar
+        '
+        Me.ButtonValidar.Location = New System.Drawing.Point(503, 12)
+        Me.ButtonValidar.Name = "ButtonValidar"
+        Me.ButtonValidar.Size = New System.Drawing.Size(143, 37)
+        Me.ButtonValidar.TabIndex = 0
+        Me.ButtonValidar.Text = "VALIDAR"
+        Me.ButtonValidar.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox2)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(434, 64)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(319, 109)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "VERTICAL"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(7, 32)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(242, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "1-Sistema operativo desarrollado por microsoft"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(7, 68)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(224, 17)
+        Me.CheckBox2.TabIndex = 1
+        Me.CheckBox2.Text = "2-Lenguaje de programacion muy utilizado"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CheckBox3)
+        Me.GroupBox2.Controls.Add(Me.CheckBox4)
+        Me.GroupBox2.Location = New System.Drawing.Point(434, 193)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(319, 109)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "HORIZONTAL"
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(7, 68)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(187, 17)
+        Me.CheckBox3.TabIndex = 1
+        Me.CheckBox3.Text = "4-Es una secuencia de comandos"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(7, 32)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(312, 17)
+        Me.CheckBox4.TabIndex = 0
+        Me.CheckBox4.Text = "3-Usado para introducir o mostrar datos de una computadora"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(107, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "3"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(107, 193)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "4"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(248, 101)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "2"
+        '
+        'Crucigrama
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(765, 344)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ButtonValidar)
+        Me.Name = "Crucigrama"
+        Me.Text = "CRUCIGRAMA"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ButtonValidar As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox4 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+End Class
---
+++ Crucigrama/Crucigrama/Crucigrama.vb
@@ -0,0 +1,59 @@
+﻿Public Class Crucigrama
+    Dim TextBoxArray(10, 10) As TextBox
+    Const TxtBoxSize As Integer = 25
+    Dim crucigrama(,) As String = {
+        {"-", "-", "-", "-", "W", "-", "-", "-", "-", "-"},
+        {"-", "-", "-", "-", "I", "-", "-", "-", "-", "-"},
+        {"-", "-", "-", "-", "N", "-", "-", "-", "-", "-"},
+        {"-", "-", "-", "-", "D", "-", "-", "-", "-", "-"},
+        {"C", "O", "N", "S", "O", "L", "A", "-", "-", "P"},
+        {"-", "-", "-", "-", "W", "-", "-", "-", "-", "Y"},
+        {"-", "-", "-", "-", "S", "C", "R", "I", "P", "T"},
+        {"-", "-", "-", "-", "-", "-", "-", "-", "-", "H"},
+        {"-", "-", "-", "-", "-", "-", "-", "-", "-", "O"},
+        {"-", "-", "-", "-", "-", "-", "-", "-", "-", "N"}
+    }
+
+    Private Sub Crucigrama_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim i, j As Integer
+        For i = 0 To 9
+            For j = 0 To 9
+                TextBoxArray(i, j) = New TextBox
+                TextBoxArray(i, j).Width = TxtBoxSize
+                TextBoxArray(i, j).Height = TxtBoxSize
+                TextBoxArray(i, j).AutoSize = False
+                TextBoxArray(i, j).MaxLength = 1
+                Console.WriteLine(crucigrama(i, j))
+                If crucigrama(i, j) <> "-" Then
+                    Console.WriteLine("se crea una caja")
+                    TextBoxArray(i, j).Location = New Point(TxtBoxSize + j * TxtBoxSize, TxtBoxSize + i * TxtBoxSize)
+                    Me.Controls.Add(TextBoxArray(i, j))
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub ButtonValidar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonValidar.Click
+        'VALIDAR CRUCIGRAMA
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+
+    Private Sub CheckBox4_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox4.CheckedChanged
+
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
